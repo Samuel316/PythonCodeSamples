653fc6f51ee12180653ff2e676b484ff1efe1f08
Samuel Lloyd <32365597+Samuel316@users.noreply.github.com>
2025-03-07 14:19:09 +0000

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,9 @@ df[df_bbti_orders.select_dtypes(include="object").columns] = (
     df.select_dtypes(include="object").apply(lambda x: x.str.upper())
 )
 
+# Reorder Column Levels
+df.pivot_table(...).reorder_levels([{New Column List}], axis=1).sort_index(axis=1)
+
 # Convert whole DataFrame to percent of Total
 
 df.div(df.sum(axis=1), axis=0).multiply(100)
@@ -18,10 +21,26 @@ df[df["Datetime"] >= pd.to_datetime("today") - pd.DateOffset(months=1)]
 # Get day of year
 df["Datetime"].dt.day_of_year
 
+# String Methods
+df["String"].str.contains(regex=True, pat=r"\d+")
+
+# Filter
+df.filter(like="String Part")
+
+# Map - Replaces Everything - can be dict or series
+df["Value"].map({"A": "a", "B", "b"})
+
+# Replace - Replaces Matches Only - Can also be a regex, string
+df["Value"].replace("A", "a)
+df["Value"].replace({"A": "a", "B", "b"})
+
 # Groupby/Resample Examples
 df.groupby("Group").size()
 df.groupby("Group").resample("h", on="Datetime").size()
 
 df.groupby("Group").agg({"index": "count", "Value": "min"})
 
+df.groupby("Group").agg(Count=("Index", "size"), ConcatString=("String", lambda x: ",".join(x)))
 
+# Background Gradient
+df..style.background_gradient()
