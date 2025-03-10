fa6f98ff80606734aae155fbf0db1acd59aeb304
Samuel Lloyd <32365597+Samuel316@users.noreply.github.com>
2025-03-10 13:30:22 +0000

    Added connection string example

--- duckdb.py
+++ duckdb.py
@@ -1,4 +1,6 @@
 # MYSQL
+duckdb.sql("INSTALL mysql; LOAD mysql;")
+duckdb.sql(f"ATTACH '{connection_string}' AS DatabaseName (TYPE MYSQL);")
 
 duckdb.sql(f"SELECT * FROM mysql_query('DatabaseName', \"{mysql_request}\")").to_df()
 
